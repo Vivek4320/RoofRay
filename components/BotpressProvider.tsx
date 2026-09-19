be4bbcdca1de74e333c39a2da8ae59e96a3f9943
Kedar Thakar <kedarthakar5278@gmail.com>
2026-09-19 18:34:00 +0530

    Load Botpress using official script order

--- components/BotpressProvider.tsx
+++ components/BotpressProvider.tsx
@@ -1,23 +1,7 @@
 'use client';
 
-import Script from "next/script";
-
-const BOTPRESS_INJECT_URL =
-  "https://cdn.botpress.cloud/webchat/v3.7/inject.js";
-const BOTPRESS_CONFIG_URL =
-  "https://files.bpcontent.cloud/2026/07/12/16/20260712163416-V9EJYWEN.js";
-
 export default function BotpressProvider() {
-  return (
-    <>
-      <Script
-        src={BOTPRESS_INJECT_URL}
-        strategy="afterInteractive"
-      />
-      <Script
-        src={BOTPRESS_CONFIG_URL}
-        strategy="afterInteractive"
-      />
-    </>
-  );
+  // Botpress is loaded directly in app/layout.tsx so the official
+  // blocking inject script always runs before the deferred config script.
+  return null;
 }
